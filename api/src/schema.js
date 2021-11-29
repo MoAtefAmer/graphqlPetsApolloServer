a60a7a37bc40bd9e229f10d6db3de9d77116d288
MoAtefAmer <mo.atefamer@gmail.com>
2021-11-29 19:38:10 +0200

    1.0

--- api/src/schema.js
+++ api/src/schema.js
@@ -1,11 +1,43 @@
-const { gql } = require('apollo-server')
+const { gql } = require('apollo-server');
 
 /**
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
+  type User {
+    id: ID!
+    username: String!
+  }
+
+  type Pet {
+    id: ID!
+    createdAt: String!
+    name: String!
+    type: String!
+  }
+
+ 
+
+  input PetInput {
+    name: String
+    type: String
+  }
+
+  type Query {
+    pets(input: PetInput): [Pet]!
+    pet(Id:String):Pet!
+  }
+
+input NewPetInput{
+    name: String!
+    type: String!
+}
+
+type Mutation{
+    newPet(input:NewPetInput!):Pet!
+
+}
 
 `;
 
-module.exports = typeDefs
+module.exports = typeDefs;
